203e0be1d72874496796d3af5bd1873eaa38d2d7
Sibusten <Sibusten@gmail.com>
2020-12-18 13:31:09 -0500

    Add CLient method to get a tag by ID

--- PhilomenaClient/IPhilomenaClient.cs
+++ PhilomenaClient/IPhilomenaClient.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using Philomena.Client.Api.Models;
+
 namespace Philomena.Client
 {
     public interface IPhilomenaClient
@@ -8,5 +11,19 @@ namespace Philomena.Client
         /// <param name="query">The search query</param>
         /// <returns></returns>
         ISearchQuery Search(string query);
+
+        /// <summary>
+        /// Gets a tag by its ID
+        /// </summary>
+        /// <param name="tagId">The ID of the tag</param>
+        /// <returns>The tag model</returns>
+        Task<TagModel> GetTagById(int tagId);
+
+        /// <summary>
+        /// Gets a tag by its name
+        /// </summary>
+        /// <param name="tagName">The name of the tag</param>
+        /// <returns>The tag model</returns>
+        // TagModel GetTagByName(string tagName)
     }
 }
--- PhilomenaClient/PhilomenaClient.cs
+++ PhilomenaClient/PhilomenaClient.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Philomena.Client.Api;
+using Philomena.Client.Api.Models;
 
 namespace Philomena.Client
 {
@@ -17,6 +20,25 @@ namespace Philomena.Client
         public ISearchQuery Search(string query)
         {
             return new SearchQuery(_api, query, ApiKey);
+        }
+
+        public async Task<TagModel> GetTagById(int tagId)
+        {
+            string tagQuery = $"id:{tagId}";
+
+            TagSearchModel tagSearch = await _api.SearchTagsAsync(tagQuery, 1, 1);
+
+            if (tagSearch.Tags is null)
+            {
+                throw new InvalidOperationException("The search query did not provide a list of tags");
+            }
+
+            if (!tagSearch.Tags.Any())
+            {
+                throw new ArgumentOutOfRangeException(nameof(tagId), tagId, "A tag with this ID was not found");
+            }
+
+            return tagSearch.Tags.First();
         }
     }
 }
